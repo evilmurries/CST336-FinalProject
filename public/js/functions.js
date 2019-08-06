1dca1d00101a160ce4acc25b57acf4b0ee5b346c
cpiwarski00 <cpiwarski00@gmail.com>
2019-08-05 23:19:41 -0400

    shopping cart

--- public/js/functions.js
+++ public/js/functions.js
@@ -18,8 +18,23 @@ $(document).ready(function() {
       success: function(result, status) {
         
         $("#animalResult").html("");
+        $("#cartContainer").html("");
         $("#animalResult").append("<img id='image' src='" + result[0].image + "' width='200' height='200'/>");
+        $("#cartContainer").append('<i class="fas fa-shopping-cart"></i>');
+        // Switch to <i class="fas fa-cart-arrow-down"></i> when clicked on
       }
     }) //ajax
   //});
 }) //ready document
+
+// Action Listener for when the shopping cart is clicked on
+$("#cartContainer").on("click", function() {
+  $("#cartContainer").html("");
+  $("#cartContainer").append('<i class="fas fa-cart-arrow-down"></i>');
+});
+
+// Action listener for when an already clicked cart is clicked
+$("#cartContainer").on("click", function() {
+  $("#cartContainer").html("");
+  $("#cartContainer").append('<i class="fas fa-shopping-cart"></i>');
+});
